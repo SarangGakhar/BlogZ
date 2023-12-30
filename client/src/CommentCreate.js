fa998cb5ec33a75b4976c27e1396ab9fb081caea
sarang <gakharsarang1@gmail.com>
2023-12-30 15:38:40 +0530

    initial commit

---
+++ client/src/CommentCreate.js
@@ -0,0 +1,19 @@
+import React, {useState} from "react";
+
+const CommentCreate = ({postId})=>{
+  const [content, setContent]=useState("");
+  return (
+    <div>
+      <form>
+        <div className="form-group ">
+          <label>New Comment</label>
+          <input value={content} onChange={e=>setContent(e.target.value)} className="form-control"/>
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default CommentCreate;
+
